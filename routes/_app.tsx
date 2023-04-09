39fc65604fc69917049e5ea900878c8123e1e7ad
Kyohei Uto <im@kyoheiu.dev>
2023-04-09 15:47:49 +0900

    Add viewport

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -2,15 +2,16 @@ import { asset, Head } from "$fresh/runtime.ts";
 import { AppProps } from "$fresh/src/server/types.ts";
 
 export default function App({ Component }: AppProps) {
-    return (
-        <html data-custom="data">
-            <Head>
-                <title>rssless</title>
-                <link rel="stylesheet" href="global.css" />
-            </Head>
-            <body>
-                <Component />
-            </body>
-        </html>
-    );
+  return (
+    <html data-custom="data">
+      <Head>
+        <title>rssless</title>
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <link rel="stylesheet" href="globals.css" />
+      </Head>
+      <body>
+        <Component />
+      </body>
+    </html>
+  );
 }
