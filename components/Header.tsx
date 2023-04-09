10cdbc0a167f058626eccdb6e8ce887363a234a7
Kyohei Uto <im@kyoheiu.dev>
2023-04-09 15:48:09 +0900

    Add header

---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+import IconRss from "https://deno.land/x/tabler_icons_tsx@0.0.3/tsx/rss.tsx";
+
+export const Header = () => {
+  return (
+    <>
+      <header>
+        <IconRss />
+      </header>
+    </>
+  );
+};
