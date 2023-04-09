9905a5d0ddb0915b8a7ab5a35b349b23600ee5e3
Kyohei Uto <im@kyoheiu.dev>
2023-04-09 15:48:28 +0900

    Fix layout

--- components/Contents.tsx
+++ components/Contents.tsx
@@ -2,59 +2,100 @@ import { Feed } from "https://deno.land/x/rss@0.5.8/mod.ts";
 import { FeedEntry } from "https://deno.land/x/rss@0.5.8/src/types/mod.ts";
 import { useState } from "https://esm.sh/preact@10.13.1/hooks";
 import { DOMParser } from "https://deno.land/x/deno_dom/deno-dom-wasm.ts";
-import { ContentElement, ParsedDescription } from "../types/typs.ts";
+import { Entry, ParsedDescription } from "../types/types.ts";
+import IconSend from "https://deno.land/x/tabler_icons_tsx@0.0.3/tsx/send.tsx";
+import { between } from "https://deno.land/x/tims/mod.ts";
 
-const asContentElement = (feed: Feed): ContentElement[] => {
-    const site = feed.title.value;
-    return feed.entries.map((x: FeedEntry) => {
-        const desc = parseDescription(x.description!.value);
+const asContentElement = (feed: Feed): Entry[] => {
+  const site = feed.title.value;
+  return feed.entries.map((x: FeedEntry) => {
+    const desc = parseDescription(x.description!.value);
 
-        return {
-            feed: site!,
-            title: x.title?.value,
-            updated: x.updated,
-            link: x.id!,
-            cover: desc.cover,
-            text: desc.text
-        };
-    });
-}
-
-const compareUpdated = (a: ContentElement, b: ContentElement): number => {
-    const elementA = a.updated ?? "0";
-    const elementB = b.updated ?? "0";
-
-    if (elementA > elementB) {
-        return -1;
-    } else if (elementB > elementA) {
-        return 1;
-    } else {
-        return 0
+    return {
+      feed: site!,
+      title: x.title?.value,
+      updated: x.updated,
+      link: x.id!,
+      cover: desc.cover,
+      text: desc.text,
     };
-}
+  });
+};
+
+const compareUpdated = (a: Entry, b: Entry): number => {
+  const elementA = a.updated ?? "0";
+  const elementB = b.updated ?? "0";
+
+  if (elementA > elementB) {
+    return -1;
+  } else if (elementB > elementA) {
+    return 1;
+  } else {
+    return 0;
+  }
+};
 
 const parseDescription = (html: string | undefined): ParsedDescription => {
-    const document = new DOMParser().parseFromString(html!, "text/html");
-    const cover = document?.querySelector("img")?.getAttribute("src");
-    const a = document?.querySelector("a");
-    a?.remove();
-    const text = document?.textContent;
-    return { cover: cover, text: text };
-}
+  const document = new DOMParser().parseFromString(html!, "text/html");
+  const cover = document?.querySelector("img")?.getAttribute("src");
+  const a = document?.querySelector("a");
+  a?.remove();
+  const text = document?.textContent;
+  return { cover: cover, text: text };
+};
 
 export const Contents = (props: { originalList: Feed[] }) => {
-    const entries: ContentElement[] = props.originalList.map((x: Feed) => { return asContentElement(x); }).flat();
-    entries.sort(compareUpdated);
+  const entries: Entry[] = props.originalList.map((x: Feed) => {
+    return asContentElement(x);
+  }).flat();
+  entries.sort(compareUpdated);
 
-    const [list, setList] = useState(entries);
+  const [list, setList] = useState(entries);
 
-    return (<>
-        <div>
-            <ul>
-                {list.map((x: ContentElement) => {
-                    return (<><li><a href={x.link} target="blank" rel="noopner noreferrer">{x.title}</a><div className="description"><img className="thumbnail" src={x.cover} />{x.text}</div></li></>);
-                })}
-            </ul >
-        </div >
-    </>);
-}
+  const diff = (now: number, updated: Date): string => {
+    return between(now, updated).split(",")[0];
+  };
+
+  return (
+    <>
+      <div className="main-list">
+        {list.map((x: Entry) => {
+          return (
+            <>
+              <div className="entry-block">
+                <div className="entry-wrapper">
+                  <div className="entry-title">
+                    <a href={x.link} target="blank" rel="noopner noreferrer">
+                      {x.title}
+                    </a>
+                  </div>
+                  <div className="entry-buttons">
+                    <IconSend size={18} />
+                  </div>
+                </div>
+                <div className="entry-info">
+                  <div>
+                    {x.feed}
+                  </div>
+                  <div>
+                    &nbsp; &nbsp;
+                  </div>
+                  <div>
+                    {diff(Date.now(), x.updated!)}
+                  </div>
+                </div>
+                <hr className="entry-divider" />
+                <div className="entry-description">
+                  <img className="entry-thumbnail" src={x.cover} />
+                  <div className="entry-text">
+                    {x.text}
+                  </div>
+                </div>
+              </div>
+            </>
+          );
+        })}
+      </div>
+    </>
+  );
+};
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,35 +1,48 @@
 import { Handlers, PageProps } from "$fresh/server.ts";
 import { Head } from "$fresh/runtime.ts";
 import * as rss from "https://deno.land/x/rss@0.5.8/mod.ts";
-import { FeedList } from "../types/typs.ts";
+import { FeedList } from "../types/types.ts";
 import { parseFeed } from "https://deno.land/x/rss@0.5.8/src/deserializer.ts";
-import { FeedsList } from "../components/feedsList.tsx";
+import { FeedsList } from "../components/FeedsList.tsx";
 import { Contents } from "../components/Contents.tsx";
+import { Header } from "../components/Header.tsx";
 
 export const handler: Handlers<rss.Feed[]> = {
   async GET(_, ctx) {
     const feedList = await Deno.readTextFile("feed.json");
     const list: FeedList = JSON.parse(feedList);
-    const resultList = await Promise.all(list.feeds.map(x => { return fetch(x); }));
-    const result = await Promise.all(resultList.map(async (x: Response) => { return await x.text() }));
-    const parsed = await Promise.all(result.map(async (x: string) => await parseFeed(x)));
+    const resultList = await Promise.all(list.feeds.map((x) => {
+      return fetch(x);
+    }));
+    const result = await Promise.all(resultList.map(async (x: Response) => {
+      return await x.text();
+    }));
+    const parsed = await Promise.all(
+      result.map(async (x: string) => await parseFeed(x)),
+    );
 
-    console.log(JSON.stringify(parsed.map(x => x.entries).flat().map(x => x.description!.value)));
+    console.log(parsed);
 
     return ctx.render(parsed);
-  }
-}
+  },
+};
 
 export default function Top({ data }: PageProps<rss.Feed[] | null>) {
   if (!data) {
     return <h1>Feeds not found.</h1>;
   } else {
-    return (<div className="wrapper"><aside>
-      <FeedsList originalList={data} />
-    </aside>
-      <main>
-        <Contents originalList={data} />
-      </main>
-    </div>);
+    return (
+      <>
+        <Header />
+        <div className="wrapper">
+          <aside>
+            <FeedsList originalList={data} />
+          </aside>
+          <main>
+            <Contents originalList={data} />
+          </main>
+        </div>
+      </>
+    );
   }
 }
