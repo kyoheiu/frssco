58fc51a7b4ccef0e66f53ddfad9424618a5af94f
Kyohei Uto <im@kyoheiu.dev>
2023-04-12 06:12:40 +0900

    Use Promise.allSettled

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,7 +1,6 @@
 import { Handlers, PageProps } from "$fresh/server.ts";
-import { Head } from "$fresh/runtime.ts";
 import * as rss from "https://deno.land/x/rss@0.5.8/mod.ts";
-import { FeedList, FeedsState } from "../types/types.ts";
+import { FeedsState } from "../types/types.ts";
 import { parseFeed } from "https://deno.land/x/rss@0.5.8/src/deserializer.ts";
 import { FeedsList } from "../components/FeedsList.tsx";
 import { Contents } from "../components/Contents.tsx";
@@ -12,25 +11,40 @@ export const handler: Handlers<FeedsState> = {
     const url = new URL(req.url);
     const q = url.searchParams.get("url");
 
-    const feedList = await Deno.readTextFile("feed.json");
-    const list: FeedList = JSON.parse(feedList);
-    const resultList = await Promise.all(list.feeds.map((x) => {
+    const text = await Deno.readTextFile("feed.txt");
+    const feedList = text.split(/\r?\n/g).filter((x: string) =>
+      !x.startsWith("#")
+    );
+    const resList = await Promise.allSettled(feedList.map((x) => {
       return fetch(x);
     }));
-    const result = await Promise.all(resultList.map(async (x: Response) => {
-      return await x.text();
-    }));
-    let parsed = await Promise.all(
-      result.map(async (x: string) => await parseFeed(x)),
+    const bodyList = await Promise.all(
+      resList.map(async (x: PromiseSettledResult<Response>) => {
+        if (x.status === "fulfilled") {
+          return await x.value.text();
+        }
+      }),
+    );
+    const parsedList = await Promise.allSettled(
+      bodyList.map(async (x: string | undefined) => {
+        if (x) {
+          return await parseFeed(x);
+        }
+      }),
     );
 
+    let result =
+      (parsedList.filter((x) =>
+        x.status === "fulfilled"
+      ) as PromiseFulfilledResult<rss.Feed>[]).map((x) => x.value);
+
     if (q) {
-      parsed = parsed.filter((x: rss.Feed) => x.id === q);
-      return ctx.render({ feeds: parsed, filtered: true });
+      result = result.filter((x: rss.Feed) => x.id === q);
+      return ctx.render({ feeds: result, filtered: true });
     }
 
-    console.log(parsed);
-    return ctx.render({ feeds: parsed, filtered: false });
+    console.log(result);
+    return ctx.render({ feeds: result, filtered: false });
   },
 };
 
