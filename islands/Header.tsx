53065730e598898ae9eab6e85ec39567135d6e3f
Kyohei Uto <im@kyoheiu.dev>
2023-04-14 05:14:22 +0900

    Add refresh button

--- islands/Header.tsx
+++ islands/Header.tsx
@@ -3,10 +3,35 @@ import { useState } from "https://esm.sh/preact@10.13.1/hooks";
 import IconMenu2 from "https://deno.land/x/tabler_icons_tsx@0.0.3/tsx/menu-2.tsx";
 import IconArrowBarToUp from "https://deno.land/x/tabler_icons_tsx@0.0.3/tsx/arrow-bar-to-up.tsx";
 import { FeedsList } from "../components/FeedsList.tsx";
-import { FeedsState } from "../types/types.ts";
+import { Entry, Feedback } from "../types/types.ts";
+import IconRefresh from "https://deno.land/x/tabler_icons_tsx@0.0.3/tsx/refresh.tsx";
+import IconRefreshDot from "https://deno.land/x/tabler_icons_tsx@0.0.3/tsx/refresh-dot.tsx";
+import IconRefreshAlert from "https://deno.land/x/tabler_icons_tsx@0.0.3/tsx/refresh-alert.tsx";
 
-export default function Header(props: { data: FeedsState }) {
+export default function Header(props: { data: Entry[] }) {
   const [showMenu, setShowMenu] = useState(false);
+  const [loading, setLoading] = useState(Feedback.Init);
+
+  const refresh = async () => {
+    setLoading(() => Feedback.Loading);
+    const res = await fetch("/api/refresh");
+    if (!res.ok) {
+      setLoading(() => Feedback.Error);
+    } else {
+      setLoading(() => Feedback.Init);
+    }
+  };
+
+  const RefreshButton = () => {
+    switch (loading) {
+      case Feedback.Init:
+        return <IconRefresh />;
+      case Feedback.Loading:
+        return <IconRefreshDot />;
+      case Feedback.Error:
+        return <IconRefreshAlert />;
+    }
+  };
 
   const toggleMenu = () => {
     setShowMenu((state) => !state);
@@ -15,7 +40,14 @@ export default function Header(props: { data: FeedsState }) {
   return (
     <>
       <header>
-        <IconRss />
+        <a href="/">
+          <IconRss />
+        </a>
+        <div class="menu-button">
+          <button onClick={refresh}>
+            <RefreshButton />
+          </button>
+        </div>
         <div class="menu-button">
           <button onClick={toggleMenu}>
             {showMenu ? <IconArrowBarToUp /> : <IconMenu2 />}
