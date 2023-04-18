0310a52d74b36ab791f201ee5fd6fc0d6623f638
Kyohei Uto <im@kyoheiu.dev>
2023-04-18 16:34:50 +0900

    Add loggedIn property to FeedsState

--- types/types.ts
+++ types/types.ts
@@ -1,30 +1,31 @@
 export interface FeedsState {
-    feeds: Entry[],
-    filtered?: string
+  feeds: Entry[];
+  filtered?: string;
+  loggedIn: boolean;
 }
 
 export interface ParsedDescription {
-    cover: string | null,
-    text: string
+  cover: string | null;
+  text: string;
 }
 
 export interface Entry {
-    sitetitle: string,
-    siteurl: string,
-    title: string,
-    link: string,
-    date: Date,
-    cover: string | null,
-    text: string
+  sitetitle: string;
+  siteurl: string;
+  title: string;
+  link: string;
+  date: Date;
+  cover: string | null;
+  text: string;
 }
 
 export interface Site {
-    sitetitle: string,
-    siteurl: string
+  sitetitle: string;
+  siteurl: string;
 }
 
 export enum Feedback {
-    Init,
-    Loading,
-    Error,
+  Init,
+  Loading,
+  Error,
 }
