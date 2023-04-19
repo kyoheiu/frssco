d1eeea45c647cd7ac5fb97e005bc1ab6800867d4
Kyohei Uto <im@kyoheiu.dev>
2023-04-20 05:57:33 +0900

    Add toast when url-copying

--- islands/CopyLink.tsx
+++ islands/CopyLink.tsx
@@ -1,4 +1,5 @@
 import IconLink from "https://deno.land/x/tabler_icons_tsx@0.0.3/tsx/link.tsx";
+import toastr from "https://esm.sh/toastr";
 
 type urlProp = {
   url: string;
@@ -6,6 +7,8 @@ type urlProp = {
 
 const copyToClipboard = async (url: string) => {
   await navigator.clipboard.writeText(url);
+  toastr.options.positionClass = "toast-bottom-right";
+  toastr.info(`URL copied: ${url}`);
 };
 
 export default function CopyLink(props: { url: string }) {
